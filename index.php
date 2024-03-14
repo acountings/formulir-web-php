f0e022c6ed43bdb86a8c2370f06cf7ba0004ef2c
acountings <ythmadog@gmail.com>
2024-03-14 15:18:56 +0700

    first Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+// include 'auth/Session.php';
+include 'layout/header.php';
+
+?>
+
+<div class="navbar navbar-expand-lg navbar-dark bg-info justify-content-center">
+    <div class="container">
+        <!-- Navbar content -->
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav justify content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">contact</a>
+                </li>
+               
+            </ul>
+        </div>
+    </div>
+</div>
+
+<section>
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</section>
+<?php
+include 'layout/footer.php';
+?>
