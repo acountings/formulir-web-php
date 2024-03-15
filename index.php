0c7cbe369bf3cafc9db5415bb69ede1dc29e0660
acountings <ythmadog@gmail.com>
2024-03-15 15:27:23 +0700

    Sudah Di buat

--- index.php
+++ index.php
@@ -9,9 +9,7 @@ include 'layout/header.php';
     <div class="container">
         <!-- Navbar content -->
         <a class="navbar-brand" href="#"><b>Evaluasi Dampak</b> Pelatihan (EDP)</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav justify content-center">
             </ul>
@@ -19,7 +17,7 @@ include 'layout/header.php';
         <div class="user-panel ">
             <div class="image">
                 <img src="layout-template/dist/img/user2-160x160.jpg" class="rounded-circle" width="30px" alt="User Image">
-                <h1> Alexandre Pirce</h1>
+                
             </div>
             <div class="info">
                 
